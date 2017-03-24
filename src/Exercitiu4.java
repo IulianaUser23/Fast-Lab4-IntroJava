48ae8117bceb027ae00707451e6291835bde12f5
IulianaUser23 <fiiuliana@yahoo.ca>
2017-03-24 19:26:49 +0200

    Adaugari

--- src/Exercitiu4.java
+++ src/Exercitiu4.java
@@ -1,15 +1,18 @@
 /**
  * Created by User on 24.03.2017.
  */
-// se citesc 3 numere, sa se afiseze cel mai mic
+// se citesc 2 numere - sa se afiseze cel mai mare
 public class Exercitiu4 {
     public static void main(String[] args) {
-    int a, b, c;
+        int a;
         a=SkeletonJava.readIntConsole("Introduceti primul numar:");
+        int b;
         b=SkeletonJava.readIntConsole("Introduceti al doilea numar:");
-        c=SkeletonJava.readIntConsole("Introduceti al treilea numar:");
-        if (a<b){
-            SkeletonJava.printConsole("numarul" + a + "este mai mare");
+
+        if (a==b) {SkeletonJava.printConsole("numerele sunt egale");}
+        else if (a>b) { SkeletonJava.printConsole("numarul" + a + "este mai mare");
         }
+        else {
+            SkeletonJava.printConsole("numarul" + b + "este mai mare");}
     }
 }
