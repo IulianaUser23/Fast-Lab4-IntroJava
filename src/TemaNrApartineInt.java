e7d2e111fdce7d501ca53d16b90366c6a79b451f
IulianaUser23 <fiiuliana@yahoo.ca>
2017-03-30 23:52:13 +0300

    Tema2Ex4

---
+++ src/TemaNrApartineInt.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Iuliana on 30.03.2017.
+ */
+//Se da un numar. Sa se afiseze ‘true' daca numarul apartine intervalului [9,24]. Altfel sa se afiseze ‘false'.
+public class TemaNrApartineInt {
+    public static void main(String[] args) {
+        int n;
+        n = SkeletonJava.readIntConsole("Introduceti numarul:");
+        if (n>=9 & n<=24) {
+            {System.out.println("True");}
+        }
+            else {System.out.println("False");}
+        }
+    }
+
